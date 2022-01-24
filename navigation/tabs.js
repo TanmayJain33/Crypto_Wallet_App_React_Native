4a6ef197efdd7c95046477b8467511acf9edb961
TanmayJain33 <tanmay.jain@crownstack.com>
2022-01-24 15:37:37 +0530

    Add Bottom Tab Navigator

--- navigation/tabs.js
+++ navigation/tabs.js
@@ -5,6 +5,8 @@ import PortfolioScreen from '../screens/PortfolioScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 import MarketScreen from '../screens/MarketScreen';
 import {COLORS} from '../constants/theme';
+import TabIcon from '../components/TabIcon';
+import icons from '../constants/icons';
 
 const Tab = createBottomTabNavigator();
 
@@ -13,15 +15,75 @@ const Tabs = () => {
     <Tab.Navigator
       screenOptions={{
         headerShown: false,
+        tabBarShowLabel: false,
         tabBarStyle: {
+          height: 140,
           backgroundColor: COLORS.primary,
+          borderTopColor: 'transparent',
         },
       }}>
-      <Tab.Screen name="Home Screen" component={HomeScreen} />
-      <Tab.Screen name="Portfolio Screen" component={PortfolioScreen} />
-      <Tab.Screen name="Trade Screen" component={HomeScreen} />
-      <Tab.Screen name="Market Screen" component={MarketScreen} />
-      <Tab.Screen name="Profile Screen" component={ProfileScreen} />
+      <Tab.Screen
+        name="Home Screen"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({focused}) => {
+            return <TabIcon focused={focused} icon={icons.home} label="Home" />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Portfolio Screen"
+        component={PortfolioScreen}
+        options={{
+          tabBarIcon: ({focused}) => {
+            return (
+              <TabIcon
+                focused={focused}
+                icon={icons.briefcase}
+                label="Portfolio"
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Trade Screen"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({focused}) => {
+            return (
+              <TabIcon
+                focused={focused}
+                icon={icons.trade}
+                label="Trade"
+                isTrade={true}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Market Screen"
+        component={MarketScreen}
+        options={{
+          tabBarIcon: ({focused}) => {
+            return (
+              <TabIcon focused={focused} icon={icons.market} label="Market" />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Profile Screen"
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: ({focused}) => {
+            return (
+              <TabIcon focused={focused} icon={icons.profile} label="Profile" />
+            );
+          },
+        }}
+      />
     </Tab.Navigator>
   );
 };
